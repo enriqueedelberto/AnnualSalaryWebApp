b6b46bb9846e360ddef0cb65ef0048460aa8c220
enriqueedelberto <enriqueviiireyes@gmail.com>
2020-11-16 15:20:49 -0500

    Progress bar

--- AnnualSalaryWebApp/Assets/FrontEnd/home.js
+++ AnnualSalaryWebApp/Assets/FrontEnd/home.js
@@ -2,39 +2,51 @@
 
 angular.module('myApp.home', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/home', { 
+        $routeProvider.when('/home', {
             controller: 'HomeCtrl'
         });
     }])
-      
+
     .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        const urlAPI = "http://localhost:8092/api/Employee/";  
+        const urlAPI = "http://localhost:8092/api/Employee/";
         $scope.lstEmployees = [];
         $scope.employee = {
             id: ''
         };
 
+        var self = this;
+        self.determinateValue = 30;
+        self.determinateValue2 = 30;
+
+        self.showList = [];
+
+
         $scope.errorMessages = [];
 
 
+        $scope.showLoadingBar = false;
 
         $scope.getEmployees = function () {
-          
+
             if ($scope.myForm.$invalid) {
                 $scope.errorMessages.push("Fields should be verified");
                 return;
             }
+
+            $scope.showLoadingBar = true;
 
             //Call Http API
             $http({
                 method: "get",
                 url: `${urlAPI}${$scope.employee.id}`
             }).then(function (response) {
+                $scope.showLoadingBar = false;
                 $scope.lstEmployees = response.data;
-            }, function () { 
-                    $scope.errorMessages.push("Error Occur");
-            }) 
+            }, function () {
+                $scope.showLoadingBar = false;
+                $scope.errorMessages.push("Error Occur");
+            })
         }
 
 
@@ -45,7 +57,7 @@ angular.module('myApp.home', ['ngRoute'])
 
             $scope.lstEmployees = [];
 
-              $scope.errorMessages = [];
+            $scope.errorMessages = [];
         }
 
         $scope.closeAlert = function () {
